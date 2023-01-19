a539b33b5c022caaabd5b5620f6ad0e8360a4856
Yie <pumasami7@gmail.col>
2023-01-19 18:25:21 +0100

    add

---
+++ test/src/App.js
@@ -0,0 +1,25 @@
+import "./yeah.css";
+import { createContext, useState } from "react";
+import ReactSwitch from "react-switch";
+
+export const ThemeContext = createContext(null);
+
+function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id={theme}>
+        <div className="switch">
+          <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+          <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+        </div>
+      </div>
+    </ThemeContext.Provider>
+  );
+}
+
+export default App;
